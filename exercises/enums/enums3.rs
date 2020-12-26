1b8e5c2e5fcd15eff61eb1ea10644a93888c4aa2
Ziyi Hua <ziyihua@Ziyis-iMac.local>
2020-12-26 14:02:18 +0000

    enums

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,6 +5,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -37,6 +41,12 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor(color) => self.change_color(color),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(p) => self.move_position(p),
+            Message::Quit => self.quit()
+        }
     }
 }
 
