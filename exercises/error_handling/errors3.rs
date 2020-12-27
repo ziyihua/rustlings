17030855dcf8101aa020ae614139cd4dea28ac04
Ziyi Hua <ziyihua@Ziyis-iMac.local>
2020-12-27 14:22:00 +0000

    error handling

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,10 +7,11 @@
 // I AM NOT DONE
 
 use std::num::ParseIntError;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     let mut tokens = 100;
-    let pretend_user_input = "8";
+    let pretend_user_input = "110";
 
     let cost = total_cost(pretend_user_input)?;
 
@@ -20,6 +21,8 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
